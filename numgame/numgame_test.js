da07b3219b688d0c949f782963db3fdb958d98cd
Evan Kling <erkling@wpi.edu>
2026-07-21 15:06:55 -0400

    many changes

--- numgame/numgame_test.js
+++ numgame/numgame_test.js
@@ -3,3 +3,7 @@ console.assert(messageText !== null, "MT null");
 console.assert(guessCountText !== null, "GCT null");
 console.assert(guessButton !== null, "GB null");
 console.assert(resetButton !== null, "RB null");
+console.assert(min === 1, "min is not 1");
+console.assert(max === 100, "max is not 100");
+console.assert(secret >= min && secret <= max, "secret is not between min and max");
+console.assert(guessCount === 0, "guessCount is not 0");
