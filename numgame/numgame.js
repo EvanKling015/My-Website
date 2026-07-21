937670d42b0c926b95241f4a8a5b0669f636d933
Matthew Schick <mrschick@wpi.edu>
2026-07-21 14:54:28 -0400

    numgame and test stuff added

---
+++ numgame/numgame.js
@@ -0,0 +1,5 @@
+const numField = document.getElementById("num-field");
+const messageText = document.getElementById("message-text");
+const guessCountText = document.getElementById("guess-count-text");
+const guessButton = document.getElementById("guess-button");
+const resetButton = document.getElementById("reset-button");
---
+++ numgame/numgame_test.js
@@ -0,0 +1,5 @@
+console.assert(numField !== null, "NF null");
+console.assert(messageText !== null, "MT null");
+console.assert(guessCountText !== null, "GCT null");
+console.assert(guessButton !== null, "GB null");
+console.assert(resetButton !== null, "RB null");
