dcf26189c49c3155b3d69542e3c403c0f0279a5e
Evan Kling <erkling@wpi.edu>
2026-07-29 14:10:30 -0400

    agdasg

--- snake-game/SnakeControls.js
+++ snake-game/SnakeControls.js
@@ -6,23 +6,25 @@ class SnakeControls{
         this.LEFT_ARROW = 'ArrowLeft';
         this.RIGHT_ARROW = 'ArrowRight';
 
-        window.addEventListener("keydown", (event) => 
-            {if ([this.UP_ARROW, this.DOWN_ARROW].includes(
-            event.code)){event.preventDefault()}});
-        window.addEventListener("keyup", (event) =>this.keyUp(event));
+        window.addEventListener("keydown", (event) => {
+            if ([this.UP_ARROW, this.DOWN_ARROW, this.LEFT_ARROW, this.RIGHT_ARROW].includes(event.key)){
+                event.preventDefault();
+            }
+            this.keyDown(event);
+        });
     }
 
-    keyUp(event){
-        if (event.key === this.UP_ARROW && (this.snake.vy <= 0)){
+    keyDown(event){
+        if (event.key === this.UP_ARROW && this.snake.vy !== 1){
             this.snake.changeDirection(0, -1);
         }
-        else if (event.key === this.DOWN_ARROW && (this.snake.vy >= 0)){
+        else if (event.key === this.DOWN_ARROW && this.snake.vy !== -1){
             this.snake.changeDirection(0, 1);
         }
-        else if (event.key === this.LEFT_ARROW && (this.snake.vx <= 0)){
+        else if (event.key === this.LEFT_ARROW && this.snake.vx !== 1){
             this.snake.changeDirection(-1, 0);
         }
-        else if (event.key === this.RIGHT_ARROW && (this.snake.vx >= 0)){
+        else if (event.key === this.RIGHT_ARROW && this.snake.vx !== -1){
             this.snake.changeDirection(1, 0);
         }
     }
